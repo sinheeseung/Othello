b0df5fe711fe3d3cb15a9b5ac5854e55c9a06ca1
heeseung <tlsgmltmd726529@gmail.com>
2021-12-04 16:00:34 +0900

    view 함수 구현

--- view.cpp
+++ view.cpp
@@ -1,6 +1,7 @@
 #include "view.h"
 #include <iostream>
-
+#include <vector>
+#include <string>
 view::view(void) {};
 
 void view::showMessage(std::string text) {
@@ -13,7 +14,13 @@ int view::InputArraySize() {
         return size;
 }
 
-void view::showArray() {
+void view::showArray(vector<vector<std::string>> arr) {
+	for (int i=0;i<arr.size(); i++){
+		for(int j=0;j<arr[i].size(); j++){
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << endl;
+	}
 }
 
 std::tuple<int, int> inputLocation() {
--- view.h
+++ view.h
@@ -1,11 +1,12 @@
 #include "contoller.h"
-
+#include <vector>
+#include <string>
 class View {
  public:
          View(void);
          void showMessage(std::string text);
          int InputArraySize(); // 처음 시작시 판의 크기 입력
-         void showArray(2차원 배열); // 현재 판 출력
+         void showArray(vector<vector<std::string>> arr); // 현재 판 출력
          int inputLocation(); // 돌을 놓을 위치 입력
 
 };
