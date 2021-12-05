4f1782df329d766a93c64d11ba37ab08d9171ab6
heeseung <tlsgmltmd726529@gmail.com>
2021-12-06 00:28:10 +0900

    main flow 작성

--- controller.cpp
+++ controller.cpp
@@ -1,31 +1,79 @@
-#include "controller.hpp"
+#include "controller.h"
+#include "model.h"
+#include "view.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
 
-Controller::Controller() {}
+Controller::Controller() {
+	insModel_ = new Model();
+	insView_ = new View();
+};
 
 void Controller::ApplicationRun() {
+	insView_->showMessage("오델로 판의 크기를 입려해주세요");
+	int size = insView_->InputArraySize();
+	insModel_->init(size);
+	insView_->showArray(insModel_->getArray());
+	bool is_end = false;
+	do {
+		//놓을 수 있는 곳이 있는지 확인
+		bool is_possible = is_Continue(true);
+		if (is_possible) {
+			insView_->showMessage("Player One이 놓을 돌의 위치를 입력하시오: (x y)");
+			std::tuple<int, int> result = insView_->inputLocation();
+			int x = std::get<0>(result);
+			int y = std::get<1>(result);
+			insModel_->setArray(x, y, true);
+			insView_->showArray(insModel_->getArray());
+			Reverse(); // 뒤집을 돌을 찾아줌
+			insView_->showArray(insModel_->getArray());
+		}
+		else 
+			insView_->showMessage("Player One이 놓을 수 있는 위치가 없습니다.");
 
+		is_possible = is_Continue(false);
+                if (is_possible) {
+                        insView_->showMessage("Player Two가 놓을 돌의 위치를 입력하시오: (x y)");
+                        std::tuple<int, int> result = insView_->inputLocation();
+                        int x = std::get<0>(result);
+                        int y = std::get<1>(result);
+                        insModel_->setArray(x, y, false);
+                        insView_->showArray(insModel_->getArray());
+                        Reverse(); // 뒤집을 돌을 찾아줌
+                        insView_->showArray(insModel_->getArray());
+                }
+                else
+                        insView_->showMessage("Player Two가 놓을 수 있는 위치가 없습니다.");
+                
+		if ((is_Continue(true) == false and is_Continue(false) == false) or is_End())
+		      is_end = true;	
+
+	}while(is_end)
+	int score = Score();
 }
 
 bool Controller::is_Possible() {
-
+	return true;
 }
 
 bool Controller::is_Reverse() {
-
+	return true;
 }
 
-??? Controller::Reserve() {
+void Controller::Reverse() {
 
 }
 
 bool Controller::is_Continue() {
-
+	return true;
 }
 
 bool Controller::is_End() {
-
+	return true;
 }
 
 int Controller::Score() {
-
+	return 0;
 }
