b8e97c6f9754b2c7121ca783b9c673b85dd32c66
heeseung <tlsgmltmd726529@gmail.com>
2021-12-06 02:20:03 +0900

    함수 구현 -ver 1

--- controller.cpp
+++ controller.cpp
@@ -6,6 +6,14 @@
 #include <string>
 #include <tuple>
 
+void ApplicationRun(); // game main flow
+bool is_Possible(); // Player가 선택한 위치에 돌을 놓을 수 있는지 확인
+void Reverse(bool is_playerone); // 돌을 뒤집는 함수
+bool is_Continue(); // Player가 돌을 놓을 수 있는곳이 있는 지확인
+bool is_End(); //종료조건 확인(판에 하나의 돌만 있는 경우)
+void Score(); //경기가 종료된 후 점수 계산
+void is_Reverse(int i, int j, bool is_playerone);
+
 Controller::Controller() {
 	insModel_ = new Model();
 	insView_ = new View();
@@ -50,18 +58,105 @@ void Controller::ApplicationRun() {
 		if ((is_Continue(true) == false and is_Continue(false) == false) or is_End())
 		      is_end = true;	
 
-	}while(is_end)
+	}while(is_end);
 	Score();
 }
 
-bool Controller::is_Possible() {
-	return true;
+bool Controller::is_Possible(int i, int j, bool is_playerone) {
+	std::vector<std::vector<std::string>> arr = insModel_->getArray();
+        int size = insModel_->getSize();
+	if(is_playerone){
+		if(i-2>=0 and j-2>=0){
+                        if(arr[i-1][j-1] == "B" and arr[i-2][j-2] == "W")
+				//왼쪽 위  대각선
+                                return true;
+                }
+                if(i-2>=0){
+                        if(arr[i-1][j] == "B" and arr[i-2][j] == "W")
+				//위쪽
+                                return true;
+                }
+                if(i-2>=0 and j+2 <size){
+                        if(arr[i-1][j+1] == "B" and arr[i-2][j+2] == "W")
+				//오른쪽 위 대각선
+                                return true;
+                }
+                if(j-2>=0){
+                        if(arr[i][j-1] == "B" and arr[i][j-2] == "W")
+				//왼쪽
+                                return true;
+                }
+		if(i+2<size and j+2<size){
+                        if(arr[i+1][j+1] == "B" and arr[i+2][j+2] == "W")
+				//오른쪽 아래 대각선
+                                return true;
+                }
+                if(i+2<size){
+                        if(arr[i+1][j] == "B" and arr[i+2][j] == "W")
+				//아래쪽
+                                return true;
+                }
+                if(i+2<size and j-2 >=0){
+                        if(arr[i+1][j-1] == "B" and arr[i+2][j-2] == "W")
+				//왼쪽 아래 대각선
+                                return true;
+                }
+                if(j+2<size){
+                        if(arr[i][j+1] == "B" and arr[i][j+2] == "W")
+				//오른쪽
+                                return true;
+                }
+	}
+	else{
+                if(i-2>=0 and j-2>=0){
+                        if(arr[i-1][j-1] == "W" and arr[i-2][j-2] == "B")
+                                //왼쪽 위  대각선
+                                return true;
+                }
+                if(i-2>=0){
+                        if(arr[i-1][j] == "W" and arr[i-2][j] == "B")
+                                //위쪽
+                                return true;
+                }
+                if(i-2>=0 and j+2 <size){
+                        if(arr[i-1][j+1] == "W" and arr[i-2][j+2] == "B")
+                                //오른쪽 위 대각선
+                                return true;
+                }
+                if(j-2>=0){
+                        if(arr[i][j-1] == "W" and arr[i][j-2] == "B")
+                                //왼쪽
+                                return true;
+                }
+                if(i+2<size and j+2<size){
+                        if(arr[i+1][j+1] == "W" and arr[i+2][j+2] == "B")
+                                //오른쪽 아래 대각선
+                                return true;
+                }
+                if(i+2<size){
+                        if(arr[i+1][j] == "W" and arr[i+2][j] == "B")
+                                //아래쪽
+                                return true;
+                }
+                if(i+2<size and j-2 >=0){
+                        if(arr[i+1][j-1] == "W" and arr[i+2][j-2] == "B")
+                                //왼쪽 아래 대각선
+                                return true;
+                }
+                if(j+2<size){
+                        if(arr[i][j+1] == "W" and arr[i][j+2] == "B")
+                                //오른쪽
+                                return true;
+                }
+        }
+	return false;
 }
 
 void Controller::Reverse(bool is_playerone) {
 	std::vector<std::vector<std::string>> arr = insModel_->getArray();
-        for (int i=0;i<arr.length();i++){
-                for( int j=0;j<arr[i].length();j++){
+        int size = insModel_->getSize();
+        for (int i=0;i<size;i++){
+                for( int j=0;j<size;j++){
 			if(is_playerone){
 				if(arr[i][j] == "B")
 					is_Reverse(i,j, is_playerone);
@@ -72,55 +167,57 @@ void Controller::Reverse(bool is_playerone) {
                 }
         }
 }
-void is_Reverse(int i, int j, bool is_playerone){
+void Controller::is_Reverse(int i, int j, bool is_playerone){
 	std::vector<std::vector<std::string>> arr = insModel_->getArray();
+        int size = insModel_->getSize();
 	if(is_playerone) {
-		if(i-1>=0 and j-1>=0 and i+1 < arr.length() and j+1 <arr.length()){
+		if(i-1>=0 and j-1>=0 and i+1 < size and j+1 <size){
 			if(arr[i-1][j-1] == "W" and arr[i+1][j+1] == "W")
 				insModel_->modifyArray(i,j);
 		}
-		if(i-1>=0 and i+1 < arr.length()){
+		if(i-1>=0 and i+1 < size){
                         if(arr[i-1][j] == "W" and arr[i+1][j] == "W")
                                 insModel_->modifyArray(i,j);
 		}
-	        if(j-1>=0 and j+1 <arr.length()){
+	        if(j-1>=0 and j+1 <size){
                         if(arr[i][j-1] == "W" and arr[i][j+1] == "W")
                                 insModel_->modifyArray(i,j);
 		}
-		if(i-1>=0 and j-1>=0 and i+1 < arr.length() and j+1 <arr.length()){
+		if(i-1>=0 and j-1>=0 and i+1 < size and j+1 <size){
                         if(arr[i-1][j+1] == "W" and arr[i+1][j-1] == "W")
                                 insModel_->modifyArray(i,j);
 		}
 	}
 	else {
-		if(i-1>=0 and j-1>=0 and i+1 < arr.length() and j+1 <arr.length()){
+		if(i-1>=0 and j-1>=0 and i+1 < size and j+1 <size){
                         if(arr[i-1][j-1] == "B" and arr[i+1][j+1] == "B")
                                 insModel_->modifyArray(i,j);
                 }
-                if(i-1>=0 and i+1 < arr.length()){
+                if(i-1>=0 and i+1 < size){
                         if(arr[i-1][j] == "B" and arr[i+1][j] == "B")
                                 insModel_->modifyArray(i,j);
                 }
-                if(j-1>=0 and j+1 <arr.length()){
+                if(j-1>=0 and j+1 <size){
                         if(arr[i][j-1] == "B" and arr[i][j+1] == "B")
                                 insModel_->modifyArray(i,j);
                 }
-                if(i-1>=0 and j-1>=0 and i+1 < arr.length() and j+1 <arr.length()){
+                if(i-1>=0 and j-1>=0 and i+1 < size and j+1 <size){
                         if(arr[i-1][j+1] == "B" and arr[i+1][j-1] == "B")
                                 insModel_->modifyArray(i,j);
                 }
 	}
 }
-bool Controller::is_Continue() {
+bool Controller::is_Continue(bool is_playerone) {
 	return true;
 }
 
 bool Controller::is_End() {
 	std::vector<std::vector<std::string>> arr = insModel_->getArray();
+        int size = insModel_->getSize();
         int one_count=0;
         int two_count=0;
-        for (int i=0;i<arr.length();i++){
-                for( int j=0;j<arr[i].length();j++){
+        for (int i=0;i<size;i++){
+                for( int j=0;j<size;j++){
                         if(arr[i][j] == "W")
                                 one_count += 1;
                         if(arr[i][j] == "B")
@@ -135,18 +232,19 @@ bool Controller::is_End() {
 
 void Controller::Score() {
 	std::vector<std::vector<std::string>> arr = insModel_->getArray();
+        int size = insModel_->getSize();
 	int one_score=0;
 	int two_score=0;
-	for (int i=0;i<arr.length();i++){
-                for( int j=0;j<arr[i].length();j++){
+	for (int i=0;i<size;i++){
+                for( int j=0;j<size;j++){
 			if(arr[i][j] == "W")
 				one_score += 1;
 			if(arr[i][j] == "B")
 				two_score += 1;
 		}
 	}
-	insView_->showMessage("Player One의 점수는 ", one_score, "입니다.");
-	insView_->showMessage("Player Two의 점수는 ", two_score, "입니다.");
+	insView_->showMessage("Player One의 점수는 " + one_score + "입니다.");
+	insView_->showMessage("Player Two의 점수는 " + two_score + "입니다.");
 	if (one_score > two_score)
 		insView_->showMessage("Player One이 승리했습니다.");
 	else
--- controller.h
+++ controller.h
@@ -14,9 +14,9 @@ class Controller {
  public:
 	 Controller();
  	 void ApplicationRun(); // game main flow
-	 bool is_Possible(); // Player가 선택한 위치에 돌을 놓을 수 있는지 확인
+	 bool is_Possible(int i, int j, bool is_playerone); // Player가 선택한 위치에 돌을 놓을 수 있는지 확인
  	 void Reverse(bool is_playerone); // 돌을 뒤집는 함수
- 	 bool is_Continue(); // Player가 돌을 놓을 수 있는곳이 있는 지확인
+ 	 bool is_Continue(bool is_playerone); // Player가 돌을 놓을 수 있는곳이 있는 지확인
  	 bool is_End(); //종료조건 확인(판에 하나의 돌만 있는 경우)
  	 void Score(); //경기가 종료된 후 점수 계산
          void is_Reverse(int i, int j, bool is_playerone);
