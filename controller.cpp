3898ad157c1339837245e819ed2343ca17fcd3ef
heeseung <tlsgmltmd726529@gmail.com>
2021-12-06 17:17:36 +0900

    cpplint

--- controller.cpp
+++ controller.cpp
@@ -6,12 +6,12 @@
 #include "model.h"
 #include "view.h"
 
-void ApplicationRun(); // game main flow
-bool is_Possible(); // Player가 선택한 위치에 돌을 놓을 수 있는지 확인
-void Reverse(int i, int j, int count, int dir, bool is_playerone); // 돌을 뒤집는 함수
-bool is_Continue(); // Player가 돌을 놓을 수 있는곳이 있는 지확인
-bool is_End(); //종료조건 확인(판에 하나의 돌만 있는 경우)
-void Score(); //경기가 종료된 후 점수 계산
+void ApplicationRun();  // game main flow
+bool is_Possible();  // Player가 선택한 위치에 돌을 놓을 수 있는지 확인
+void Reverse(int i, int j, int count, int dir, bool is_playerone);  // 돌을 뒤집는 함수
+bool is_Continue();  // Player가 돌을 놓을 수 있는곳이 있는 지확인
+bool is_End();  //종료조건 확인(판에 하나의 돌만 있는 경우)
+void Score();  //경기가 종료된 후 점수 계산
 std::tuple<int, int> is_Reverse(int i, int j, int dir);
 int check(int i, int j, int dir, bool is_playerone);
 
@@ -31,7 +31,7 @@ void Controller::ApplicationRun() {
                 bool is_possible = is_Continue(true);
                 bool put = false;
                 if (is_possible) {
-                        do{
+                        do {
                                 insView_->showMessage("");
                                 insView_->showMessage("Player One이 놓을 돌의 위치를 입력하시오: (x y)");
                                 std::tuple<int, int> result = insView_->inputLocation();
@@ -40,7 +40,6 @@ void Controller::ApplicationRun() {
                                 if (x >= 0 && x < size && y >= 0 && y < size) {
                                         bool is_locatable = is_Possible(x, y, true, false);
                                         if (is_locatable) {
-                                                //insModel_->setArray(x, y, true);
                                                 put = true;
                                         } else {
                                                 insView_->showMessage("자신과 상대편 돌 사이에만 돌을 놓을 수 있습니다");
@@ -50,16 +49,15 @@ void Controller::ApplicationRun() {
                                         insView_->showMessage("잘못된 범위의 값을 입력하였습니다.");
                                         insView_->showMessage("1~"+std::to_string(size)+" 사이의 값을 입력해주세요.");
                                 }
-                        } while(!put);
-                        //Reverse(true); // 뒤집을 돌을 찾아줌
+                        } while (!put);
                         insView_->showArray(insModel_->getArray());
-                } else 
+                } else
                         insView_->showMessage("Player One이 놓을 수 있는 위치가 없습니다.");
 
                 is_possible = is_Continue(false);
                 put = false;
                 if (is_possible) {
-                        do{
+                        do {
                                 insView_->showMessage("");
                                 insView_->showMessage("Player Two가 놓을 돌의 위치를 입력하시오: (x y)");
                                 std::tuple<int, int> result = insView_->inputLocation();
@@ -68,8 +66,7 @@ void Controller::ApplicationRun() {
                                 if (x >= 0 && x < size && y >= 0 && y < size) {
                                         bool is_locatable = is_Possible(x, y, false, false);
                                         if (is_locatable) {
-                                                //insModel_->setArray(x, y, false);
-                                                put = true;
+                                               put = true;
                                         } else {
                                                 insView_->showMessage("자신과 상대편 돌 사이에만 돌을 놓을 수 있습니다");
                                         }
@@ -79,44 +76,41 @@ void Controller::ApplicationRun() {
                                         insView_->showMessage("잘못된 범위의 값을 입력하였습니다.");
                                         insView_->showMessage("1~"+std::to_string(size)+" 사이의 값을 입력해주세요.");
                                 }
-                        } while(!put);
-                        //Reverse(false); // 뒤집을 돌을 찾아줌
+                        } while (!put);
                         insView_->showArray(insModel_->getArray());
-                } else 
+                } else
                         insView_->showMessage("Player Two가 놓을 수 있는 위치가 없습니다.");
 
-                if ((is_Continue(true) == false && is_Continue(false) == false) or is_End())
-                        is_end = true;	
-
-        } while(!is_end);
+                if ((is_Continue(true) == false && is_Continue(false) == false) || is_End())
+                        is_end = true;
+        } while (!is_end);
         Score();
 }
 int Controller::check(int j, int i, int dir, bool is_playerone) {
         std::vector<std::vector<std::string>> arr = insModel_->getArray();
         int size = insModel_->getSize();
         int count = 0;
-        while(1) {
+        while (1) {
                 std::tuple<int, int> result = is_Reverse(i, j, dir);
                 i = std::get<0>(result);
                 j = std::get<1>(result);
                 if (i < 0 || j < 0 || i >= size || j >= size) break;
                 if (arr[i][j] == "O") break;
                 if (is_playerone) {
-                        if(arr[i][j] == "B"){
+                        if (arr[i][j] == "B") {
                                 count++;
-                        }
-                        else if(arr[i][j] == "W" && count != 0) {
+                        } else if (arr[i][j] == "W" && count != 0) {
                                 return count;
                         }
                 } else {
-                        if(arr[i][j] == "W") count++;
-                        else if(arr[i][j] == "B" && count != 0) return count;
+                        if (arr[i][j] == "W") count++;
+                        else if (arr[i][j] == "B" && count != 0) return count;
                 }
         }
         return 0;
 }
 bool Controller::is_Possible(int j, int i, bool is_playerone, bool is_con) {
-	std::vector<std::vector<std::string>> arr = insModel_->getArray();
+        std::vector<std::vector<std::string>> arr = insModel_->getArray();
         int size = insModel_->getSize();
         if (arr[i][j] != "O") return false;
         for (int dir = 0; dir < 8; dir++) {
@@ -133,20 +127,19 @@ bool Controller::is_Possible(int j, int i, bool is_playerone, bool is_con) {
 }
 
 void Controller::Reverse(int j, int i, int count, int dir, bool is_playerone) {
-	std::vector<std::vector<std::string>> arr = insModel_->getArray();
+        std::vector<std::vector<std::string>> arr = insModel_->getArray();
         int size = insModel_->getSize();
         int reverse = 0;
-        while(reverse < count) {
+        while (reverse < count) {
                 std::tuple<int, int> result = is_Reverse(i, j, dir);
                 i = std::get<0>(result);
                 j = std::get<1>(result);
-                insModel_->modifyArray(i,j);
+                insModel_->modifyArray(i, j);
                 reverse++;
         }
 }
-std::tuple<int, int> Controller::is_Reverse(int i, int j, int dir){
-	switch (dir)
-        {
+std::tuple<int, int> Controller::is_Reverse(int i, int j, int dir) {
+        switch (dir) {
         case 0:
         //왼쪽 위
                 j--;
@@ -182,23 +175,23 @@ std::tuple<int, int> Controller::is_Reverse(int i, int j, int dir){
                 //오른쪽 아래
                         j++;
                         i++;
-                        break;     
+                        break;
                 default:
                         break;
                 }
         return {i, j};
 }
 bool Controller::is_Continue(bool is_playerone) {
-	std::vector<std::vector<std::string>> arr = insModel_->getArray();
+        std::vector<std::vector<std::string>> arr = insModel_->getArray();
         int size = insModel_->getSize();
         bool ret;
         for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
                         if (arr[i][j] == "O") {
                                 ret = is_Possible(i, j, is_playerone, true);
-                                if (ret){
+                                if (ret) {
                                         return true;
-                                }        
+                                }
                         }
                 }
         }
@@ -206,41 +199,41 @@ bool Controller::is_Continue(bool is_playerone) {
 }
 
 bool Controller::is_End() {
-	std::vector<std::vector<std::string>> arr = insModel_->getArray();
+        std::vector<std::vector<std::string>> arr = insModel_->getArray();
         int size = insModel_->getSize();
-        int one_count=0;
-        int two_count=0;
-        for (int i=0;i<size;i++){
-                for( int j=0;j<size;j++){
-                        if(arr[i][j] == "W")
+        int one_count = 0;
+        int two_count = 0;
+        for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                        if (arr[i][j] == "W")
                                 one_count += 1;
-                        if(arr[i][j] == "B")
+                        if (arr[i][j] == "B")
                                 two_count += 1;
                 }
         }
-	if (one_count == 0 or two_count == 0)
-		return true;
-	else
-		return false;
+        if (one_count == 0 || two_count == 0)
+                return true;
+        else
+                return false;
 }
 
 void Controller::Score() {
-	std::vector<std::vector<std::string>> arr = insModel_->getArray();
+        std::vector<std::vector<std::string>> arr = insModel_->getArray();
         int size = insModel_->getSize();
-	int one_score=0;
-	int two_score=0;
-	for (int i=0;i<size;i++){
-                for( int j=0;j<size;j++){
-			if(arr[i][j] == "W")
-				one_score += 1;
-			if(arr[i][j] == "B")
-				two_score += 1;
-		}
-	}
-	insView_->showMessage("Player One의 점수는 " + std::to_string(one_score) +  "입니다.");
-	insView_->showMessage("Player Two의 점수는 " + std::to_string(two_score) + "입니다.");
-	if (one_score > two_score)
-		insView_->showMessage("Player One이 승리했습니다.");
-	else
-		insView_->showMessage("Player Two가 승리했습니다.");
+        int one_score = 0;
+        int two_score = 0;
+        for (int i = 0; i < size; i++){
+                for( int j = 0; j < size; j++){
+                        if (arr[i][j] == "W")
+                                one_score += 1;
+                        if (arr[i][j] == "B")
+                                two_score += 1;
+                }
+        }
+        insView_->showMessage("Player One의 점수는 " + std::to_string(one_score) +  "입니다.");
+        insView_->showMessage("Player Two의 점수는 " + std::to_string(two_score) + "입니다.");
+        if (one_score > two_score)
+                insView_->showMessage("Player One이 승리했습니다.");
+        else
+                insView_->showMessage("Player Two가 승리했습니다.");
 }
